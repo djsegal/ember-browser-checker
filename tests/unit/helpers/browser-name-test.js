85369be1ef35844646152a8a950502f50e65cc6d
djsegal <djsegal@wisc.edu>
2016-08-09 21:05:47 -0400

    Remove stubbed test (and live with jshint error)

--- tests/unit/helpers/browser-name-test.js
+++ tests/unit/helpers/browser-name-test.js
@@ -4,7 +4,3 @@ import { module, test } from 'qunit';
 module('Unit | Helper | browser name');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let result = browserName([42]);
-  assert.ok(result);
-});
